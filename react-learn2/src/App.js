110175e704fea59089de27633244940bc0ef6cf2
chenguochao <chenguochao@phone580.com>
2020-12-07 19:06:52 +0800

    feat(compose): 理解compose

--- react-learn2/src/App.js
+++ react-learn2/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import ReduxPage from './component/ReduxPage';
-
+import './utils/compose'
 function App() {
   return (
     <div className="App">
---
+++ react-learn2/src/utils/compose.js
@@ -0,0 +1,31 @@
+function f1 (arg) {
+  console.log('f1', arg)
+  return arg
+}
+function f2 (arg) {
+  console.log('f2', arg)
+  return arg
+}
+function f3 (arg) {
+  console.log('f3', arg)
+  return arg
+}
+// let res = f1(f2(f3('go'))) // f3, f2, f1
+
+// reduce改写，compose函数
+const compose = (...fns) => {
+  return fns.reduce((accutor, current) => { 
+    // accutor: a(b(c(args))), current: a(args)， b(args), c(args),
+    // 之所以要返回一个函数，是为了传参，延迟执行，之后进行包裹，将传参传到底部
+    console.log(accutor, '--accutor')
+    console.log(current, '--current')
+    return (...args) => {
+      return accutor(current(...args))
+    }
+  })
+}
+let res = compose(f1,f2,f3)('go') // f3, f2, f1
+
+
+console.log(res)
+
